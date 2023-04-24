4d9deaa3ed1a206469006a4d8ef7476201536d4e
jocy0412 <jakob1@naver.com>
2023-04-24 19:58:55 +0900

    vue store 모듈화

---
+++ src/store/mutations.js
@@ -0,0 +1,27 @@
+const addOneItem = (state, payload) => {
+  const { text } = payload;
+  const addObj = { completed: false, item: text };
+  localStorage.setItem(text, JSON.stringify(addObj));
+  state.todoItems.push(addObj);
+};
+
+const removeOneItem = (state, payload) => {
+  const { todoItem, index } = payload;
+  localStorage.removeItem(todoItem.item);
+  state.todoItems.splice(index, 1);
+};
+
+const toggleOneItem = (state, payload) => {
+  const { todoItem, index } = payload;
+  // todoItem.completed = !todoItem.completed;
+  state.todoItems[index].completed = !state.todoItems[index].completed;
+  localStorage.removeItem(todoItem.item);
+  localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+};
+
+const clearAllItems = (state) => {
+  state.todoItems = [];
+  localStorage.clear();
+};
+
+export { addOneItem, removeOneItem, toggleOneItem, clearAllItems };
--- src/store/store.js
+++ src/store/store.js
@@ -1,51 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+// import * as getters from "./getters";
+// import * as mutations from "./mutations";
+import todoApp from "./modules/todoApp";
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-            }
-        }
-        return arr;
-    },
-};
-
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch(),
-    },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        },
-    },
-    mutations: {
-        addOneItem(state, payload) {
-            const { text } = payload;
-            const addObj = { completed: false, item: text };
-            localStorage.setItem(text, JSON.stringify(addObj));
-            state.todoItems.push(addObj);
-        },
-        removeOneItem(state, payload) {
-            const { todoItem, index } = payload;
-            localStorage.removeItem(todoItem.item);
-            state.todoItems.splice(index, 1);
-        },
-        toggleOneItem(state, payload) {
-            const { todoItem, index } = payload;
-            // todoItem.completed = !todoItem.completed;
-            state.todoItems[index].completed = !state.todoItems[index].completed;
-            localStorage.removeItem(todoItem.item);
-            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-        },
-        clearAllItems(state) {
-            state.todoItems = [];
-            localStorage.clear();
-        },
-    },
+  modules: {
+    todoApp,
+  },
 });
